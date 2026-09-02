453aee11595a50872fa53c794d3d2229eb7da37c
Arnis Vuskans <ampatspell@gmail.com>
2026-09-03 01:06:06 +0300

    validated variants

--- src/lib/services.ts
+++ src/lib/services.ts
@@ -1,6 +1,4 @@
 import { createServiceGetters } from '@ampatspell/tiny/server/services/handle';
 import type { DB } from './schema';
 
-const { getDatabase, getFiles, getStorage, getUsers } = createServiceGetters<DB>();
-
-export { getDatabase, getFiles, getStorage, getUsers };
+export const { getDatabase, getFiles, getStorage, getUsers } = createServiceGetters<DB>();
