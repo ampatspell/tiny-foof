8e08d605705c611afadf63669752388705a60fd5
Arnis Vuskans <ampatspell@gmail.com>
2026-09-03 17:39:41 +0300

    simpler message.remote

--- src/lib/message/message.remote.ts
+++ src/lib/message/message.remote.ts
@@ -1,7 +1,7 @@
 import { getDatabase, getFiles } from '#lib/services.js';
 import { command, query } from '$app/server';
 import { uid } from '@ampatspell/tiny/server/utils';
-import { hasKeys, omit } from '@ampatspell/tiny/utils/object';
+import { omit } from '@ampatspell/tiny/utils/object';
 import type { QueryResponse } from '@ampatspell/tiny/utils/utils';
 import { readFile } from 'fs/promises';
 import { fileURLToPath } from 'url';
@@ -44,26 +44,22 @@ export const updateMessage = command(
     message: v.optional(v.string()),
     background: v.optional(v.strictObject({ file: v.optional(v.file()) })),
   }),
-  async (arg) => {
-    const props = omit(arg, ['background']);
-    const { background } = arg;
+  async (props) => {
     const db = getDatabase();
+    const { background } = props;
+    const rest = omit(props, ['background']);
+
+    let backgroundId;
     if (background) {
-      const files = getFiles();
-      const record = await db.selectFrom('messages').selectAll().executeTakeFirstOrThrow();
-      if (record.backgroundId) {
-        await files.drop(record.backgroundId);
-      }
-      let backgroundId = null;
-      if (background.file) {
-        backgroundId = uid();
-        await files.store(backgroundId, background.file);
-      }
-      await db.updateTable('messages').set({ backgroundId }).execute();
+      const message = await db.selectFrom('messages').select('backgroundId').executeTakeFirstOrThrow();
+      backgroundId = await getFiles().replace(message.backgroundId, uid(), background.file);
     }
-    if (hasKeys(props)) {
-      await db.updateTable('messages').set(props).execute();
-    }
+
+    await db
+      .updateTable('messages')
+      .set({ ...rest, backgroundId })
+      .execute();
+
     getMessage().refresh();
   },
 );
